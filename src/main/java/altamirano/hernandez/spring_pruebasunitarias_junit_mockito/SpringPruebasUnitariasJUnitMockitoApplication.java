5eaca217a4c149328594e9f14ed444122066ec74
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-06-02 18:29:58 -0600

    initial commit

---
+++ src/main/java/altamirano/hernandez/spring_pruebasunitarias_junit_mockito/SpringPruebasUnitariasJUnitMockitoApplication.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.spring_pruebasunitarias_junit_mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPruebasUnitariasJUnitMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPruebasUnitariasJUnitMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/altamirano/hernandez/spring_pruebasunitarias_junit_mockito/SpringPruebasUnitariasJUnitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.spring_pruebasunitarias_junit_mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPruebasUnitariasJUnitMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
